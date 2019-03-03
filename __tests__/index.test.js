cf031dda2fd19641938303234897573d72a83cff
logan <logan70@163.com>
2019-03-03 14:39:26 +0800

    test: 单元测试demo

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
-import umdName from '../src/index.js'
+import { add } from '../src/index.js'
 
 describe('单元测试(js)', () => {
   it('1加2等于3', () => {
-    expect(umdName.add(1, 2)).toEqual(3)
+    expect(add(1, 2)).toEqual(3)
   })
 })
--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,7 +1,7 @@
-import umdName from '../src/index'
+import { add } from '../src/index'
 
 describe('单元测试(ts)', () => {
   it('1加2等于3', () => {
-    expect(umdName.add(1, 2)).toEqual(3)
+    expect(add(1, 2)).toEqual(3)
   })
 })
